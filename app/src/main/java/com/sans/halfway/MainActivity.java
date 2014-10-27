b2bedfd007f9315c453be42f9169bec21c91df7a
unknown <sansless@gmail.com>
2014-10-27 02:18:35 +0100

    correct appname, correct firebase implementation

--- app/src/main/java/com/sans/halfway/MainActivity.java
+++ app/src/main/java/com/sans/halfway/MainActivity.java
@@ -8,8 +8,6 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
